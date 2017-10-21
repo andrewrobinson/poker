577fa3d54b0bc2f5ed8f35baac054172d5f12532
Andrew Robinson <andrewmcrobinson@gmail.com>
2017-10-21 23:15:28 +0200

    implement Card#buildFromShortCode

--- src/main/java/com/inclusivity/poker/Card.java
+++ src/main/java/com/inclusivity/poker/Card.java
@@ -1,10 +1,13 @@
 package com.inclusivity.poker;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * A Card has Rank and Suit
  *
- * It can be built from shortCodes such as "AS, 10C, 10H, 3D, 3S", usind buildFromShortCode
+ * It can be built from shortCodes such as "AS, 10C, 10H, 3D, 3S", using buildFromShortCode
  *
  * Internally, this uses buildFromRankCodeAndSuitCode and a shortCode of "10C"
  * becomes rankCode of "10" and suitCode of "C"
@@ -15,17 +18,48 @@ public class Card {
     Rank rank;
     Suit suit;
 
+    //This looks for a number or A, J, Q or K as the first group (the rank)
+    //Then it looks for (S or C or H or D) as the second group (the suit)
+    final static String regex = "(\\d+|A|J|Q|K)(S|C|H|D)(.*)";
+    final static Pattern pattern = Pattern.compile(regex);
+
     public Card(Rank rank, Suit suit) {
         this.rank = rank;
         this.suit = suit;
     }
 
-//    public static Card buildFromShortCode(String shortCode) {
-//
-////        "AS, 10C, 10H, 3D, 3S"
-////        return new Card(Rank.fromShortCode(rankCode), Suit.fromShortCode(suitCode));
-//
-//    }
+    public static Card buildFromShortCode(String shortCode) {
+
+        if (shortCode == null) {
+            return null;
+        }
+
+        Matcher matcher = pattern.matcher(shortCode);
+
+        if (matcher.find()) {
+
+            if (matcher.groupCount() == 3) {
+
+                //The 3rd group should be empty
+                //TODO - probabl a more regex way of ensuring nothing more is at the end
+                if (!matcher.group(3).isEmpty()) {
+                    //eg if someone passes JH1234 instead of just JH
+                    return null;
+                }
+
+                String rankCode = matcher.group(1);
+                String suitCode = matcher.group(2);
+                return new Card(Rank.fromShortCode(rankCode), Suit.fromShortCode(suitCode));
+
+            } else {
+                return null;
+            }
+
+        } else {
+            return null;
+        }
+
+    }
 
 
     protected static Card buildFromRankCodeAndSuitCode(String rankCode, String suitCode) {
--- src/test/java/com/inclusivity/poker/TestCard.java
+++ src/test/java/com/inclusivity/poker/TestCard.java
@@ -4,35 +4,46 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TestCard {
 
+    Card aceOfSpaces = new Card(Rank.ACE, Suit.SPADES);
+    Card tenOfClubs = new Card(Rank.TEN, Suit.CLUBS);
+    Card tenOfHearts = new Card(Rank.TEN, Suit.HEARTS);
+    Card threeOfDiamonds = new Card(Rank.THREE, Suit.DIAMONDS);
+    Card threeOfSpades = new Card(Rank.THREE, Suit.SPADES);
+    Card jackOfHearts = new Card(Rank.JACK, Suit.HEARTS);
+
     @Test
     public void testBuildCardFromShortCode() {
-//        "AS, 10C, 10H, 3D, 3S"
+
+        assertTrue(aceOfSpaces.equals(Card.buildFromShortCode("AS")));
+        assertTrue(tenOfClubs.equals(Card.buildFromShortCode("10C")));
+        assertTrue(tenOfHearts.equals(Card.buildFromShortCode("10H")));
+        assertTrue(threeOfDiamonds.equals(Card.buildFromShortCode("3D")));
+        assertTrue(threeOfSpades.equals(Card.buildFromShortCode("3S")));
+        assertTrue(jackOfHearts.equals(Card.buildFromShortCode("JH")));
+
+        //There shouldn't be anything extra after the rank and suit
+        assertNull(Card.buildFromShortCode("JH1234"));
+
+        assertNull(Card.buildFromShortCode(""));
+        assertNull(Card.buildFromShortCode(null));
+        assertNull(Card.buildFromShortCode("something"));
+
 
     }
 
 
     @Test
     public void testBuildCardFromRankCodeAndSuitCode() {
-//        "AS, 10C, 10H, 3D, 3S"
 
-        Card aceOfSpaces = new Card(Rank.ACE, Suit.SPADES);
         assertTrue(aceOfSpaces.equals(Card.buildFromRankCodeAndSuitCode("A", "S")));
-
-        Card tenOfClubs = new Card(Rank.TEN, Suit.CLUBS);
         assertTrue(tenOfClubs.equals(Card.buildFromRankCodeAndSuitCode("10", "C")));
-
-        Card tenOfHearts = new Card(Rank.TEN, Suit.HEARTS);
         assertTrue(tenOfHearts.equals(Card.buildFromRankCodeAndSuitCode("10", "H")));
-
-        Card threeOfDiamonds = new Card(Rank.THREE, Suit.DIAMONDS);
         assertTrue(threeOfDiamonds.equals(Card.buildFromRankCodeAndSuitCode("3", "D")));
-
-        Card threeOfSpades = new Card(Rank.THREE, Suit.SPADES);
         assertTrue(threeOfSpades.equals(Card.buildFromRankCodeAndSuitCode("3", "S")));
-
 
     }
 
